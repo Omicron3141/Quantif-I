ed177c858d9fa18e7d7c577245d400056a1fa916
Jonah Rubin <jonah@jonaherubin.com>
2018-01-31 17:40:42 -0800

    made the experiment index and view pages good

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ https://stackoverflow.com/questions/17147431/ruby-on-rails-joining-multiple-tabl
 http://guides.rubyonrails.org/active_record_basics.html - model setup
 https://stackoverflow.com/questions/4736397/default-values-for-model-fields-in-a-ruby-on-rails-form
 https://stackoverflow.com/questions/1580805/how-to-set-the-default-value-for-a-datetime-column-in-migration-script
+https://stackoverflow.com/questions/9503554/ruby-substring-to-a-certain-length-and-also-to-last-whitespace-within-substring
+https://www.ruby-forum.com/topic/78603
+https://stackoverflow.com/questions/21710486/rails-bootstrap-how-to-format-form-for-width-grid-collapses
 
 
 
--- quantifi/app/controllers/datapoints_controller.rb
+++ quantifi/app/controllers/datapoints_controller.rb
@@ -4,6 +4,26 @@ class DatapointsController < ApplicationController
 	  @datapoint = @experiment.datapoints.create(datapoint_params)
 	  redirect_to experiment_path(@experiment)
     end
+
+    def destroy
+	  @experiment = Experiment.find(params[:experiment_id])
+      @datapoint = @experiment.datapoints.find(params[:id])
+      @datapoint.destroy
+      redirect_to experiment_path(@experiment)
+	end
+
+	def edit
+        @datapoint = Datapoint.find(params[:id])
+	end
+  	def update
+	  @datapoint = Datapoint.find(params[:id])
+	 
+	  if @datapoint.update(datapoint_params)
+	    redirect_to @datapoint.experiment
+	  else
+	    render 'edit'
+	  end
+	end
 	 
     private
       def datapoint_params

5ef4d3aa15ff5374dc6c9b3ff57142b01f0c5160
kcwoolverton <kcwoolverton@gmail.com>
2018-02-03 17:20:12 -0800

    Added passwords to Users

--- quantifi/app/controllers/users_controller.rb
+++ quantifi/app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       redirect_to @user
     else
@@ -44,6 +44,6 @@ class UsersController < ApplicationController
 
     private
       def user_params
-        params.require(:user).permit(:name, :email)
+        params.require(:user).permit(:name, :email, :password)
       end
 end
--- quantifi/app/models/user.rb
+++ quantifi/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :experiments, dependent: :destroy
   validates :name, :email, presence: true
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end

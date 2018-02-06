8c97279d2ae430cede7307fa246692b07595bb7d
kcwoolverton <kcwoolverton@gmail.com>
2018-02-06 15:55:33 -0800

    Added login page (login still not working)

--- quantifi/config/routes.rb
+++ quantifi/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'homepage/index'
 
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users do
     resources :experiments do
       resources :datapoints

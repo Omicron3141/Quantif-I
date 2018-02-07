4186be144950a0795acd2d7cdea4c215d7edaad2
kcwoolverton <kcwoolverton@gmail.com>
2018-02-06 16:05:06 -0800

    Added basic login

--- quantifi/config/routes.rb
+++ quantifi/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'homepage/index'
 
-  get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'

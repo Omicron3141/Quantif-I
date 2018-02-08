2618a6c621fd9281113511bed8c5227aff33fe19
kcwoolverton <kcwoolverton@gmail.com>
2018-02-08 15:18:17 -0800

    Added logout

--- quantifi/app/controllers/sessions_controller.rb
+++ quantifi/app/controllers/sessions_controller.rb
@@ -13,11 +13,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  def logout
-    log_out
-    redirect_to root_url
-  end 
-
   def destroy
     log_out
     redirect_to root_url

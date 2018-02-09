4da263425a896cf66c3f66492ec122f9e154322d
kcwoolverton <kcwoolverton@gmail.com>
2018-02-08 20:52:18 -0800

    Added maximum length for username and password

--- quantifi/app/models/user.rb
+++ quantifi/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :experiments, dependent: :destroy
-  validates :name, :email, presence: true
+  validates :name, :email, presence: true, length: {maximum: 25}
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 256}
 end
